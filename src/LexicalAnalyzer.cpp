fa5da8531d69e2f1df5c51e3c0a70447d9dfc855
Caden <przcaden@gmail.com>
2021-03-05 01:02:19 -0600

    error fixes

--- src/LexicalAnalyzer.cpp
+++ src/LexicalAnalyzer.cpp
@@ -1,9 +1,8 @@
 
-
+#include<iostream>
 #include <string>
 #include <vector>
 #include <algorithm>
-#include <iostream>
 #include <fstream>
 #include <map>
 using namespace std;
@@ -16,14 +15,12 @@ private:
 	// other private methods
 public:
 
-	LexAnalyzer(istream& infile) {
+	LexAnalyzer(ifstream& infile) {
 		string token;
 		string lexeme;
 		infile >> token >> lexeme;
 		while (!infile.eof()) {
-			//m[lexeme] = token;
-			tokenmap.insert(lexeme, token);
-			tokenmap.
+			tokenmap[lexeme] = token;
 
 			infile >> token >> lexeme;
 		}
@@ -32,17 +29,16 @@ public:
 	// lexeme pairs i.e.  s_and and t_begin begin t_int 27.  Each pair    // appears on its own input line.
 	// post: tokenmap has been populated
 
-	bool contains(map<string, string>& m, string lex) {
+	bool contains(string lex) {
 		map<string, string>::iterator mitr;
 		bool exists = false;
-		for (mitr=m.begin(); mitr!=m.end(); mitr++) { // if you are using an iterator to iterate through a data structure, do not change the size.
-			if (*mitr == lex)
+		for (mitr=tokenmap.begin(); mitr!=tokenmap.end(); mitr++) { // if you are using an iterator to iterate through a data structure, do not change the size.
+			if (exists)
 				exists = true;
 		}
 		return exists;
 	}
-	// pre: 1st parameter refers to a vector that contains all lexemes
-	// gotten from tokenlexemedata.txt, 2nd parameter refers to a lexeme
+	// pre: parameter refers to a string holding a lexeme
 	// received from a source code file.
 	// post: returns true if the vector contains the lexeme string, false if not.
 
@@ -78,18 +74,18 @@ public:
 		bool loop = !infile.eof();
 		while (loop) {
 			lexemes.push_back(lex);
-			if (contains(tokenmap, lex)) {
+			if (contains(lex)) {
 
 			}
 		}
 
-		for (int i=0; i<lexemes(); i++) {
+		for (int i=0; i<lexemes.size(); i++) {
 			lex = lexemes[i];
 
 			if (i==lexemes.size()-1 && !loop)
 				outfile << lex << " : error: invalid token." << endl;
-			else
-				outfile << tokenmap[lex] << " : " << lex << endl;
+			//else
+				//outfile << tokenmap[lex] << " : " << lex << endl;
 		}
 	}
 	// pre: 1st parameter refers to an open text file that contains source
@@ -113,20 +109,9 @@ int main() {
 	cin >> codename;
 
 	ifstream codefile(codename);
-	//ostream outfile;
-	//analyzer.scanFile(codefile, outfile);
+	ostream outfile;
+	analyzer.scanFile(codefile, outfile);
 
-	map<string, int> m; // standard template data structure.
-	m["hello"] = 12;
-	m["today"] = 15;
-	cout << m["hello"] << endl;
-
-	vector<int>::iterator vitr;
-	vector<int> v;
-	for (vitr=v.begin(); vitr!=v.end(); vitr++) {
-		cout << *vitr << endl; // print value vitr is on
-	}
-	return 0;
 }
 
 /*
